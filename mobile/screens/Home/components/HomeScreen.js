6d9d8394326524e1437d77f20193a1ec8c992ad1
Lucas S <lucas.soaresouza@gmail.com>
2019-10-18 21:35:26 -0300

    Adding the google maps api

--- mobile/screens/Home/components/HomeScreen.js
+++ mobile/screens/Home/components/HomeScreen.js
@@ -1,20 +1,50 @@
 import React from 'react';
+import MapView from 'react-native-maps';
 import { View, Text, Container } from 'native-base';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import FooterBar, { tabScreens } from '../../../shared/components/FooterBar';
+import { StyleSheet, Dimensions } from 'react-native';
 
 class HomeScreen extends React.Component {
+
+  state = {
+    camera: {
+      center:{
+        latitude: -15.98932261,
+        longitude: -48.04509044,
+      },
+      altitude: 1216,
+      pitch: 3,
+      heading: 3,
+      zoom: 17,
+    },
+  };
+
   render() {
     return (
-      <Container style={{ backgroundColor: '#FFF' }}>
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>Home Screen</Text>
-        </View>
-        <FooterBar screen={tabScreens.home} />
-      </Container>
+      <View style={styles.container}>
+        <MapView
+          style={styles.mapStyle}
+          provider={MapView.PROVIDER_GOOGLE}
+          onRegionChange={this._handleMapRegionChange}
+          camera={this.state.camera}/>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  mapStyle:{
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+});
 
 export default HomeScreen;
