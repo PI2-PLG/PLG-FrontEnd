db18f657245797998754837f7df29ab258027fe1
Taynara Carvalho <tayhcarvalho@gmail.com>
2019-10-26 22:17:14 -0300

    Creating segment

--- mobile/screens/Graphics/components/BarChart.js
+++ mobile/screens/Graphics/components/BarChart.js
@@ -1,19 +1,23 @@
 import React from "react";
 import { Container, Content, Card, CardItem, Body, Text } from 'native-base';
-import { VictoryBar, VictoryChart } from "victory-native";
+import { VictoryBar, VictoryChart, VictoryAxis } from "victory-native";
 
 export default class BarChart extends React.Component {
     render(){
         const { data, width, x, y, theme, title} = this.props;
         return(
             <Content>
-              <Card style={{padding: 0}}>
-                  <CardItem header style={{ alignSelf: "center"}}>
-                    <Text>{title}</Text>
+              <Card style={{ borderRadius: 20 }}>
+                  <CardItem header style={{ alignSelf: "center", borderRadius: 20}}>
+                    <Text style={{color: '#525252'}}>{title}</Text>
                   </CardItem>
-                <CardItem>
-                     <VictoryChart width={width} theme={theme} domainPadding={{ x: 25 }} height={200} padding={{left: 50, bottom: 30}}>
-                        <VictoryBar data={data} x={x} y={y} 
+                <CardItem style={{borderRadius: 20}}>
+                     <VictoryChart width={width} theme={theme} domainPadding={{ x: 25 }} height={150} padding={{left: 50, bottom: 30, top: 10}}
+                     >
+                        <VictoryBar 
+                            data={data} 
+                            x={x} 
+                            y={y}
                             style={{ 
                                 data:{ 
                                     fill: '#F2CABA',
--- mobile/screens/Graphics/components/GraphicsScreen.js
+++ mobile/screens/Graphics/components/GraphicsScreen.js
@@ -2,16 +2,15 @@ import React from "react";
 import { StyleSheet, View, ScrollView } from "react-native";
 import { VictoryBar, VictoryChart, VictoryTheme, VictoryPie, VictoryLine } from "victory-native";
 import FooterBar, { tabScreens } from '../../../shared/components/FooterBar';
-// import {
-//   LineChart,
-//   BarChart,
-//   PieChart,
-//   ProgressChart,
-//   ContributionGraph,
-//   StackedBarChart
-// } from "react-native-chart-kit";
+import {
+  LineChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
 import BarChart from './BarChart'
-import { Container } from "native-base";
+import { Container, Header, Left, Segment, Button, Body, Text } from "native-base";
 
 const data = [
   { quarter: 1, earnings: 13000 },
@@ -21,99 +20,132 @@ const data = [
 ];
 
 export default class GraphicsScren extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.props = props;
+
+    this.state = {
+        seg: 1,
+    }
+  }
   render() {
     const data = [
-      { quarter: 1, earnings: 13000 },
-      { quarter: 2, earnings: 16500 },
-      { quarter: 3, earnings: 14250 },
-      { quarter: 4, earnings: 19000 }
+      { quarter: 'seg', earnings: 26 },
+      { quarter: 'ter', earnings: 25 },
+      { quarter: 'qua', earnings: 28 },
+      { quarter: 'qui', earnings: 25 },
+      { quarter: 'sex', earnings: 30 }
+    ];
+    const data2 = [
+      { quarter: 'seg', earnings: 26 },
+      { quarter: 'ter', earnings: 25 },
+      { quarter: 'quar', earnings: 28 },
+      { quarter: 'qui', earnings: 25 },
+      { quarter: 'sex', earnings: 30 }
     ];
 
     return (
       <Container>
-      <ScrollView>
-        <View style={styles.container}>
-          <BarChart
-            data={data}
-            width={300}
-            x='quarter'
-            y = 'earnings'
-            theme={VictoryTheme.material}
-            title = 'Quarter x Earnings'
-          >
-          </BarChart>
-          {/* <BarChart
-            data={data}
-            width={300}
-            x='quarter'
-            y = 'earnings'
-            theme={VictoryTheme.material}
-          >
-          </BarChart>
-          <BarChart
-            data={data}
-            width={300}
-            x='quarter'
-            y = 'earnings'
-            theme={VictoryTheme.material}
-          >
-          </BarChart> */}
-          {/* <VictoryPie
-            animate={{
-              duration: 2000
+        <Segment style={{marginTop: 30, backgroundColor: 'white'}}>
+          <Button
+            style={{
+              backgroundColor: this.state.seg === 1 ? "#DD6E42" : undefined,
+              borderColor: "#DD6E42",
             }}
-            colorScale={["tomato", "orange", "gold", "cyan", "navy" ]}
-            labels={({ datum }) => datum.y}
-            data={[
-              { x: "Cats", y: 35 },
-              { x: "Dogs", y: 40 },
-              { x: "Birds", y: 55 }
-            ]}
-          />
-  <LineChart
-    data={{
-      labels: ["January", "February", "March", "April", "May", "June"],
-      datasets: [
-        {
-          data: [
-            Math.random() * 100,
-            Math.random() * 100,
-            Math.random() * 100,
-            Math.random() * 100,
-            Math.random() * 100,
-            Math.random() * 100
-          ]
+            first
+            active={this.state.seg === 1 ? true : false}
+            onPress={() => this.setState({ seg: 1 })}
+          >
+            <Text style={{ color: this.state.seg === 1 ? "#FFF" : "#DD6E42" }}>Gráficos</Text>
+          </Button>
+          <Button
+            last
+            style={{
+              backgroundColor: this.state.seg === 2 ? "#DD6E42" : undefined,
+              borderColor: "#DD6E42",
+            }}
+            active={this.state.seg === 2 ? true : false}
+            onPress={() => this.setState({ seg: 2 })}
+          >
+            <Text style={{ color: this.state.seg === 2 ? "#FFF" : "#DD6E42" }}>Histórico</Text>
+          </Button>
+        </Segment>
+        { this.state.seg === 1 ?
+          (<ScrollView>
+            <View style={styles.container}>
+              <BarChart
+                data={data}
+                width={300}
+                x='quarter'
+                y = 'earnings'
+                theme={VictoryTheme.material}
+                title = 'Temperatura x Dias'
+              >
+              </BarChart>
+              <BarChart
+                data={data}
+                width={300}
+                x='quarter'
+                y = 'earnings'
+                theme={VictoryTheme.material}
+                title = 'Umidade x Dias'
+              >
+              </BarChart>
+              
+              <LineChart
+                data={{
+                  labels: ["January", "February", "March", "April", "May", "June"],
+                  datasets: [
+                    {
+                      data: [
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100,
+                        Math.random() * 100
+                      ]
+                    }
+                  ]
+                }}
+                width={310} // from react-native
+                height={220}
+                yAxisLabel={"$"}
+                yAxisSuffix={"k"}
+                chartConfig={{
+                  backgroundColor: "#e26a00",
+                  backgroundGradientFrom: "#fb8c00",
+                  backgroundGradientTo: "#ffa726",
+                  decimalPlaces: 2, // optional, defaults to 2dp
+                  color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                  labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                  style: {
+                    borderRadius: 16
+                  },
+                  propsForDots: {
+                    r: "6",
+                    strokeWidth: "2",
+                    stroke: "#ffa726"
+                  }
+                }}
+                bezier
+                style={{
+                  marginVertical: 8,
+                  borderRadius: 16
+                }}
+              />
+            </View>
+          </ScrollView>
+          ): (
+            <Container style={{ backgroundColor: '#FFF' }}>
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+              <Text> Alguma coisa aqui </Text>
+            </View>
+          </Container>
+          )
         }
-      ]
-    }}
-    width={310} // from react-native
-    height={220}
-    yAxisLabel={"$"}
-    yAxisSuffix={"k"}
-    chartConfig={{
-      backgroundColor: "#e26a00",
-      backgroundGradientFrom: "#fb8c00",
-      backgroundGradientTo: "#ffa726",
-      decimalPlaces: 2, // optional, defaults to 2dp
-      color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-      labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-      style: {
-        borderRadius: 16
-      },
-      propsForDots: {
-        r: "6",
-        strokeWidth: "2",
-        stroke: "#ffa726"
-      }
-    }}
-    bezier
-    style={{
-      marginVertical: 8,
-      borderRadius: 16
-    }}
-  /> */}
-        </View>
-      </ScrollView>
       <FooterBar screen={tabScreens.graphics} />
       </Container>
     );
@@ -126,6 +158,6 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "white",
-    paddingTop: 35
+    paddingTop: 10
   }
 });
